39c3fef6ca587a0ed8e6fec77c93fba3c8d23e30
Wamy <25761442+Wamy-Dev@users.noreply.github.com>
2025-06-25 18:40:47 -0500

    feat: adds retries to http transport

--- library/http.py
+++ library/http.py
@@ -4,6 +4,10 @@ from library.torbox import TORBOX_API_KEY
 TORBOX_API_URL = "https://api.torbox.app/v1/api"
 TORBOX_SEARCH_API_URL = "https://search-api.torbox.app"
 
+transport = httpx.HTTPTransport(
+    retries=5
+)
+
 api_http_client = httpx.Client(
     base_url=TORBOX_API_URL,
     headers={
@@ -12,6 +16,7 @@ api_http_client = httpx.Client(
     },
     timeout=httpx.Timeout(60),
     follow_redirects=True,
+    transport=transport,
 )
 
 search_api_http_client = httpx.Client(
@@ -22,6 +27,7 @@ search_api_http_client = httpx.Client(
     },
     timeout=httpx.Timeout(60),
     follow_redirects=True,
+    transport=transport,
 )
 
 general_http_client = httpx.Client(
@@ -31,4 +37,5 @@ general_http_client = httpx.Client(
     },
     timeout=httpx.Timeout(60),
     follow_redirects=False,
+    transport=transport,
 )
