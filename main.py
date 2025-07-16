f5a0f7abc412c8ec878a56a5a018c9dbede04995
Lexi <deejaymadd@gmail.com>
2025-07-16 10:00:36 +1000

    fix: symlink db

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from sys import platform
 import os
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     format='%(asctime)s,%(msecs)03d %(name)s %(levelname)s %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
 )
